f64cba77cc8b69cab965c774cd955c2abc4aebf1
haleydek <hcd09@live.com>
2019-12-16 18:34:05 -0600

    add fetch request to fetchGames action creator.

--- src/actions/fetchGames.js
+++ src/actions/fetchGames.js
@@ -1,3 +1,15 @@
 // http://www.giantbomb.com/api/search?api_key=[YOUR-KEY]&format=[RESPONSE-DATA-FORMAT]&query=[YOUR-SEARCH]&resources=[SOME-TYPES]
-let searchInput;
-const searchGamesURL = `https://www.giantbomb.com/api/search/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&query=${searchInput}&resources=game&field_list=id,guid,name,image,api_detail_url,deck,platforms`;
+
+export const fetchGames = (searchInput) => {
+    const searchGamesURL = `https://www.giantbomb.com/api/search/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&query=${searchInput}&resources=game&field_list=id,guid,name,image,api_detail_url,deck,platforms`;
+
+    return dispatch => {
+        dispatch({ type: 'LOADING_GAMES' })
+        fetch(searchGamesURL)
+        .then(res => res.json())
+        .then(resJSON => {
+            console.log(resJSON)
+            return dispatch({type: 'ADD_GAMES', payload: resJSON})
+        })
+    }
+}
