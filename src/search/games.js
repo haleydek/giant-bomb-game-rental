a561242ee8b72bdba8e642bbd0632d288085a047
haleydek <hcd09@live.com>
2020-01-10 11:49:28 -0600

    render gameCards in bootstrap rows and columns

--- src/search/games.js
+++ src/search/games.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-//import Row from 'react-bootstrap/Row';
-//import CardGroup from 'react-bootstrap/CardGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 import GameCard from './gameCard';
+import generate from '@babel/generator';
 
 class Games extends React.Component {
-    render() {
-        const { games } = this.props;
-
-        const cards = [];
+    generateGameCards = (games, numOfColumns) => {
+        const columnWidth = Math.floor(12 / numOfColumns);
+        const cards = []
 
         for (let key in games) {
             cards.push(
-                <GameCard 
-                    key={key}
-                    title={games[key].name}
-                    description={games[key].deck}
-                    image={games[key].image.small_url}
-                    detailsLink={games[key].api_detail_url}
-                />
-            )
+                <Col sm={columnWidth}>
+                    <GameCard 
+                        key={key}
+                        title={games[key].name}
+                        description={games[key].deck}
+                        image={games[key].image.small_url}
+                        detailsLink={games[key].api_detail_url}
+                    />
+                </Col>
+            );
         }
 
+        return cards;
+    }
+
+    render() {
+        const { games } = this.props;
+        const numOfColumns = 3;
+        const gameCards = this.generateGameCards(games, numOfColumns);
+
         return (
-            <Container>
-                {cards}
-            </Container>
+            <Container>{
+                gameCards.map((gameCard, idx) => {
+                    if ((idx + numOfColumns) % numOfColumns === 0) {
+                        return (
+                            <Row>{
+                                gameCards.slice(idx, (idx + numOfColumns)).map(card => card)
+                            }</Row>
+                        )
+                    }
+                })
+            }</Container>
         )
     }
     
