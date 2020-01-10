fab8423d35c68eb705df2a7b089b341c694da775
haleydek <hcd09@live.com>
2020-01-10 11:50:22 -0600

    wrap gameCard image in div to control height. Hide image overflow.

--- src/search/gameCard.js
+++ src/search/gameCard.js
@@ -3,12 +3,14 @@ import Card from 'react-bootstrap/Card';
 
 const GameCard = (props) => {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img
-                variant="top"
-                src={props.image}
-                fluid
-            />
+        <Card style={{ width: '100%' }}>
+            <div style={{ height: '14rem', overflow: 'hidden' }}>
+                <Card.Img
+                    variant='top'
+                    src={props.image}
+                    fluid
+                />
+            </div>
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>{props.description}</Card.Text>
